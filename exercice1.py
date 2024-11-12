e7aba48e24d15d8f19b9db47c00a1c1744a3add3
HichamTIERCE <hicham.tierce@hotmail.fr>
2024-11-12 09:51:01 +0100

    Vrai exercice 1 TP4

--- exercice1.py
+++ exercice1.py
@@ -1,4 +1,27 @@
-class Chien:
+class Polynome:
+
+    def __init__(self,p):
+        self.coef=p
 
     def afficher(self):
-        print('je suis un chien')
+        print(f'{self.coef[len(self.coef)-1]}X^{len(self.coef)-1}',end='')
+        i = len(self.coef)-2
+        while i>0:
+            if (self.coef[i]<0):
+                print(f' - {-self.coef[i]}X^{i}',end='')
+            else:
+                print(f' + {self.coef[i]}X^{i}',end='')
+            i-=1
+        if (self.coef[0]<0):
+            print(f' - {-self.coef[0]}',end='')
+        else:
+            print(f' + {self.coef[0]}',end='')
+        print('')
+
+    def get_valeur(self,x):
+        resultat=0
+        for i in range(0, len(self.coef)):
+            resultat=resultat+self.coef[i]*x**i
+        return resultat
+
+    
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from exercice1 import Chien
+from exercice1 import Polynome
 
-c=Chien()
-c.afficher()
+p=Polynome([1.2,-0.1,-1.3,0.1,0.1])
+print('Le polynome a cette forme : ')
+p.afficher()
+print('Valeur pour x=2 : ', end='')
+print(p.get_valeur(2))
+print('Valeur pour x=1 : ', end='')
+print(p.get_valeur(1))
+print('Valeur pour x=4 : ', end='')
+print(p.get_valeur(4))
